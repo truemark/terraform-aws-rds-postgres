65d9037a7efbb646ad9ac24ed4fa4f583e3752af
Leonard Runyowa <lrunyowa@truemark.io>
2024-04-11 19:33:22 +0200

    Common postgres - Leonard

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "aws_kms_key" "db" {
   key_id = var.kms_key_id
 }
 
-resource "random_password" "db" {
+resource "random_password" "root_password" {
   count   = var.create_db_instance ? 1 : 0
   length  = var.random_password_length
   special = false
@@ -70,23 +70,18 @@ module "db" {
   # https://registry.terraform.io/modules/terraform-aws-modules/rds/aws/latest
   source                                = "terraform-aws-modules/rds/aws"
   version                               = "6.5.4"
+
+  allocated_storage                     = var.allocated_storage
+  apply_immediately                     = var.apply_immediately
+  auto_minor_version_upgrade            = var.auto_minor_version_upgrade
+  backup_retention_period               = var.backup_retention_period
+  ca_cert_identifier                    = var.ca_cert_identifier
+  copy_tags_to_snapshot                 = var.copy_tags_to_snapshot
   create_db_instance                    = var.create_db_instance
   create_db_parameter_group             = true
-  parameters                            = var.db_parameters
-  db_name                               = var.database_name
-  allocated_storage                     = var.allocated_storage
-  max_allocated_storage                 = var.max_allocated_storage
-  storage_type                          = var.storage_type
-  iops                                  = var.iops
-  storage_encrypted                     = true
-  kms_key_id                            = var.kms_key_arn != null ? var.kms_key_arn : (var.kms_key_id != null) ? join("", data.aws_kms_key.db.*.arn) : (var.kms_key_alias != null) ? join("", data.aws_kms_alias.db.*.target_key_arn) : null
-  auto_minor_version_upgrade            = var.auto_minor_version_upgrade
-  apply_immediately                     = var.apply_immediately
-  backup_retention_period               = var.backup_retention_period
-  copy_tags_to_snapshot                 = var.copy_tags_to_snapshot
   create_db_subnet_group                = true
-  #create_random_password                = false
   db_instance_tags                      = var.tags
+  db_name                               = var.database_name
   db_subnet_group_tags                  = var.tags
   deletion_protection                   = var.deletion_protection
   engine                                = "postgres"
@@ -94,24 +89,30 @@ module "db" {
   family                                = var.family
   identifier                            = var.instance_name
   instance_class                        = var.instance_type
+  iops                                  = var.iops
+  kms_key_id                            = var.kms_key_arn != null ? var.kms_key_arn : (var.kms_key_id != null) ? join("", data.aws_kms_key.db.*.arn) : (var.kms_key_alias != null) ? join("", data.aws_kms_alias.db.*.target_key_arn) : null
+  max_allocated_storage                 = var.max_allocated_storage
   monitoring_interval                   = var.monitoring_interval
+  monitoring_role_arn                   = join("", aws_iam_role.rds_enhanced_monitoring.*.arn)
   multi_az                              = var.multi_az
-  password                              = join("", random_password.db.*.result)
+  parameters                            = var.db_parameters
+  #password                              = join("", random_password.db.*.result)
+  password                              = var.store_master_password_as_secret ? random_password.root_password.result : null
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
   skip_final_snapshot                   = var.skip_final_snapshot
   snapshot_identifier                   = var.snapshot_identifier
+  storage_encrypted                     = true
+  storage_type                          = var.storage_type
   subnet_ids                            = var.subnet_ids
   tags                                  = var.tags
   username                              = var.username
   vpc_security_group_ids                = [join("", aws_security_group.db.*.id)]
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  monitoring_role_arn                   = join("", aws_iam_role.rds_enhanced_monitoring.*.arn)
-  ca_cert_identifier                    = var.ca_cert_identifier
 }
 
 module "master_secret" {
   source   = "truemark/rds-secret/aws"
-  version  = "1.0.3"
+  version  = "1.1.2"
   create = var.create_db_instance && var.create_secrets
   cluster = false
   identifier = module.db.db_instance_id
@@ -124,7 +125,7 @@ module "master_secret" {
 module "user_secrets" {
   for_each = {for user in var.additional_users: user.username => user}
   source   = "truemark/rds-secret/aws"
-  version  = "1.0.3"
+  version  = "1.1.2"
   create = var.create_db_instance && var.create_secrets
   cluster = false
   identifier = module.db.db_instance_id
--- variables.tf
+++ variables.tf
@@ -1,25 +1,39 @@
-variable "create_db_instance" {
-  description = "Whether to create the database instance"
+variable "additional_users" {
+  description = "Additional users to generate secrets for"
+  type = list(object({
+    username      = string
+    database_name = string
+  }))
+  default = []
+}
+
+variable "allocated_storage" {
+  description = "Storage size in GB."
+  default     = 100
+}
+
+variable "apply_immediately" {
+  description = "Determines whether or not any DB modifications are applied immediately, or during the maintenance window"
   type        = bool
   default     = true
 }
 
-variable "ingress_cidrs" {
-  description = "List of allowed CIDRs that can access this RDS instance."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
+variable "auto_minor_version_upgrade" {
+  description = "Indicates that minor engine upgrades will be applied automatically to the DB instance during the maintenance window."
+  type        = bool
+  default     = false
 }
 
-variable "egress_cidrs" {
-  description = "List of allowed CIDRs that this RDS instance can access."
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
+variable "backup_retention_period" {
+  description = "How long to keep backups for (in days)"
+  type        = number
+  default     = 7
 }
 
-variable "tags" {
-  description = "A map of tags to add to all resources."
-  type        = map(string)
-  default     = {}
+variable "ca_cert_identifier" {
+  description = "The identifier of the CA certificate for the DB instance"
+  type        = string
+  default     = "rds-ca-rsa2048-g1"
 }
 
 variable "copy_tags_to_snapshot" {
@@ -28,21 +42,10 @@ variable "copy_tags_to_snapshot" {
   default     = false
 }
 
-variable "create_secrets" {
-  description = "Toggle on or off storing passwords in AWS Secrets Manager."
+variable "create_db_instance" {
+  description = "Whether to create the database instance"
   type        = bool
   default     = true
-}
-
-variable "random_password_length" {
-  description = "The length of the password to generate for root user."
-  type        = number
-  default     = 16
-}
-
-variable "username" {
-  description = "The master database account to create."
-  default     = "postgres" # This is the default username RDS uses
 }
 
 variable "create_security_group" {
@@ -51,42 +54,86 @@ variable "create_security_group" {
   default     = true
 }
 
-variable "security_group_tags" {
-  description = "Additional tags for the security group"
-  type        = map(string)
+variable "create_secrets" {
+  description = "Toggle on or off storing passwords in AWS Secrets Manager."
+  type        = bool
+  default     = true
+}
+
+variable "database_name" {
+  description = "Name for the database to be created."
+  type        = string
+  default     = null
+}
+
+variable "db_parameter_group_tags" {
+  description = "A map of tags to add to the aws_db_parameter_group resource if one is created."
   default     = {}
 }
 
-variable "vpc_id" {
-  description = "The ID of the VPC to provision into"
-  type        = string
+variable "db_parameters" {
+  description = "Map of parameters to use in the aws_db_parameter_group resource"
+  type        = list(map(any))
+  default     = []
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs to use"
+variable "db_subnet_group_name" {
+  description = "The name of the subnet group name (existing or created)"
+  type        = string
+  default     = " "
+}
+
+variable "deletion_protection" {
+  description = "Sets deletion protection on the instance"
+  type        = bool
+  default     = false
+}
+
+variable "egress_cidrs" {
+  description = "List of allowed CIDRs that this RDS instance can access."
   type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
 
-variable "allocated_storage" {
-  description = "Storage size in GB."
-  default     = 100
-}
-
-variable "max_allocated_storage" {
-  description = "Maximum storage size in GB."
-  default     = 500
-}
-
-variable "storage_type" {
-  description = "One of 'standard', 'gp2' or 'io1'."
+variable "engine_version" {
+  description = "PostgreSQL database engine version."
   type        = string
-  default     = "io1"
+  default     = "14.4"
+}
+
+variable "family" {
+  description = "The DB parameter group family name"
+  type        = string
+  default     = "postgres14"
 }
 
 variable "iops" {
   description = "The amount of provisioned IOPS. Setting implies a storage_type of io1."
   type        = number
   default     = 500
+}
+
+variable "ingress_cidrs" {
+  description = "List of allowed CIDRs that can access this RDS instance."
+  type        = list(string)
+  default     = ["10.0.0.0/8"]
+}
+
+variable "instance_name" {
+  description = "Name for the db instance. This will display in the console."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type to use at master instance. If instance_type_replica is not set it will use the same type for replica instances"
+  type        = string
+  default     = "db.t4g.small"
+}
+
+variable "kms_key_alias" {
+  description = "The alias of the KMS key to use."
+  type        = string
+  default     = null
 }
 
 variable "kms_key_arn" {
@@ -101,65 +148,39 @@ variable "kms_key_id" {
   default     = null
 }
 
-variable "kms_key_alias" {
-  description = "The alias of the KMS key to use."
-  type        = string
-  default     = null
-}
-
-variable "db_parameters" {
-  description = "Map of parameters to use in the aws_db_parameter_group resource"
-  type        = list(map(any))
-  default     = []
-}
-
-variable "db_parameter_group_tags" {
-  description = "A map of tags to add to the aws_db_parameter_group resource if one is created."
-  default     = {}
-}
-
-variable "instance_name" {
-  description = "Name for the db instance. This will display in the console."
-  type        = string
-}
-
-# aws rds describe-db-engine-versions --query "DBEngineVersions[].DBParameterGroupFamily"
-variable "family" {
-  description = "The DB parameter group family name"
-  type        = string
-  default     = "postgres14"
-}
-
-variable "engine_version" {
-  description = "PostgreSQL database engine version."
-  type        = string
-  default     = "14.4"
-}
-
-variable "instance_type" {
-  description = "Instance type to use at master instance. If instance_type_replica is not set it will use the same type for replica instances"
-  type        = string
-  default     = "db.t4g.small"
-}
-
-variable "apply_immediately" {
-  description = "Determines whether or not any DB modifications are applied immediately, or during the maintenance window"
+variable "manage_master_user_password" {
+  description = "Set to true to allow RDS to manage the master user password in Secrets Manager"
   type        = bool
   default     = true
 }
 
-variable "skip_final_snapshot" {
-  description = "Should a final snapshot be created on cluster destroy"
+variable "master_user_secret_kms_key_id" {
+  description = "The Amazon Web Services KMS key identifier is the key ARN, key ID, alias ARN, or alias name for the KMS key"
+  type        = string
+  default     = null
+}
+variable "max_allocated_storage" {
+  description = "Maximum storage size in GB."
+  default     = 500
+}
+
+variable "monitoring_interval" {
+  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance. To disable collecting Enhanced Monitoring metrics, specify 0."
+  type        = number
+  default     = 60
+}
+
+variable "multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ."
   type        = bool
   default     = false
 }
 
-variable "backup_retention_period" {
-  description = "How long to keep backups for (in days)"
+variable "performance_insights_retention_period" {
+  description = "Amount of time in days to retain Performance Insights data. Either 7 (7 days) or 731 (2 years)"
   type        = number
-  default     = 7
+  default     = null
 }
-
 variable "preferred_backup_window" {
   description = "When to perform DB backups"
   type        = string
@@ -172,8 +193,20 @@ variable "preferred_maintenance_window" {
   default     = "sat:03:00-sat:05:00" # 8PM-10PM MST
 }
 
-variable "deletion_protection" {
-  description = "Sets deletion protection on the instance"
+variable "random_password_length" {
+  description = "The length of the password to generate for root user."
+  type        = number
+  default     = 16
+}
+
+variable "security_group_tags" {
+  description = "Additional tags for the security group"
+  type        = map(string)
+  default     = {}
+}
+
+variable "skip_final_snapshot" {
+  description = "Should a final snapshot be created on cluster destroy"
   type        = bool
   default     = false
 }
@@ -184,41 +217,40 @@ variable "snapshot_identifier" {
   default     = null
 }
 
-variable "monitoring_interval" {
-  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance. To disable collecting Enhanced Monitoring metrics, specify 0."
-  type        = number
-  default     = 60
+variable "storage_type" {
+  description = "One of 'standard', 'gp2' or 'io1'."
+  type        = string
+  default     = "io1"
 }
 
-variable "auto_minor_version_upgrade" {
-  description = "Indicates that minor engine upgrades will be applied automatically to the DB instance during the maintenance window."
+variable "store_master_password_as_secret" {
+  description = "Set to true to allow self-management of the master user password in Secrets Manager"
   type        = bool
   default     = false
 }
-
-variable "multi_az" {
-  description = "Specifies if the RDS instance is multi-AZ."
-  type        = bool
-  default     = false
+variable "subnet_ids" {
+  description = "List of subnet IDs to use"
+  type        = list(string)
 }
 
-variable "database_name" {
-  description = "Name for the database to be created."
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "username" {
+  description = "The master database account to create."
+  default     = "postgres" # This is the default username RDS uses
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to provision into"
   type        = string
-  default     = null
 }
 
-variable "additional_users" {
-  description = "Additional users to generate secrets for"
-  type = list(object({
-    username      = string
-    database_name = string
-  }))
-  default = []
-}
-
-variable "ca_cert_identifier" {
-  description = "The identifier of the CA certificate for the DB instance"
-  type        = string
-  default     = "rds-ca-rsa2048-g1"
+variable "vpc_security_group_ids" {
+  description = "List of VPC security groups to associate to the cluster in addition to the security group created"
+  type        = list(string)
+  default     = []
 }
